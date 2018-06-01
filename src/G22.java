4aafa79cece87d516123b841c1df5e0b43387ed7
wkq <wukeqin@qq.com>
2018-06-01 08:51:36 +0800

    我我哦我

---
+++ src/G22.java
@@ -0,0 +1,18 @@
+
+/**  
+* <p>Title: G22</p>  
+* <p>Description: </p>  
+* @author shenlan  
+* @date 2018Äê5ÔÂ31ÈÕ  
+*/
+public class G22 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("dgfdg");
+
+	}
+
+}
